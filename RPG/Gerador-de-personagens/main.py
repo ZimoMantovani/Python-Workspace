e4e162c85ebb4103f6ac769c5dc4f2cc9df63d4e
Symon Mantovani <symonmantovani36@gmail.com>
2025-06-23 16:09:11 -0300

    Refatorar o gerador de caracteres: padronizar nomes de variáveis, adicionar funções de geração de atributos e equipamentos e aprimorar a exibição da saída

--- RPG/Gerador-de-personagens/main.py
+++ RPG/Gerador-de-personagens/main.py
@@ -7,7 +7,7 @@ print("Bem vindo ao gerador de personagens!\n obs: Nessa primeira versão você 
 fake = Faker()
 nomes_fantasia = [fake.unique.first_name() for _ in range(20)]
 nome = random.choice(nomes_fantasia)  # escolhe um nome aleatório da lista
-print(nome)
+
 
 
 racas = [
@@ -38,7 +38,7 @@ classes = [
     "Patrulheiro"
 ]
 
-Atributos = [
+atributos = [
     "Força",
     "Destreza",
     "Constituição",
@@ -47,7 +47,7 @@ Atributos = [
     "Carisma"
 ]
 
-Alinhamento = [
+alinhamento = [
     "Leal e Bom",
     "Neutro e Bom",
     "Caótico e Bom",
@@ -59,7 +59,7 @@ Alinhamento = [
     "Caótico e Mau"
 ]
 
-Historico = [
+antecedente = [
     "Acólito",
     "Artífice",
     "Criminoso",
@@ -72,15 +72,70 @@ Historico = [
     "Outlander"
 ]
 
+pericias = [
+    "Acrobacia",
+    "Arcanismo",
+    "Atletismo",
+    "Atuação",
+    "Enganação",
+    "Furtividade",
+    "História",
+    "Intimidação",
+    "Intuição",
+    "Investigação",
+    "Lidar com Animais",
+    "Medicina",
+    "Natureza",
+    "Percepção",
+    "Persuasão",
+    "Prestidigitação",
+    "Religião",
+    "Sobrevivência"
+]
 
+equipamento_inicial = [
+    "Espada curta",
+    "Adaga",
+    "Arco curto",
+    "Machado de batalha",
+    "Escudo",
+    "Armadura de couro",
+    "Armadura de malha",
+    "Poção de cura",
+    "Kit de explorador",
+    "Kit de ladrão",
+    "Livro de magias",
+    "Instrumento musical",
+    "Símbolo sagrado",
+    "Pacote de aventureiro",
+    "Rações de viagem",
+    "Tocha",
+    "Corda",
+    "Mapa antigo",
+    "Moeda de ouro",
+    "Anel misterioso"
+]
+
+
+def gerar_atributo(): # Função para gerar um valor de atributo aleatório de 8 à 15.
+    return random.randint(8, 15) 
+
+def gerar_pericia(): # Escolhe 4 pericias da lista para se tornar proficiente (sample para não repetir)
+    return random.sample(pericias, 4)
+
+def gerar_equipamento():# Escolhe 6 equipamentos da lista 
+    return random.sample(equipamento_inicial,6)
+
+periciasEscolhidas = gerar_pericia()
+equipamentoEscolhido = gerar_equipamento()
+
+#Escolhe randomicamente o basico
 raca_escolhida = random.choice(racas)
 classe_escolhida = random.choice(classes)
-historico_escolhido = random.choice(Historico)
-alinhamento_escolhido = random.choice(Alinhamento)
+antecedente_escolhido = random.choice(antecedente)
+alinhamento_escolhido = random.choice(alinhamento)
 
-def gerar_atributo():
-    return random.randint(8, 15)
-
+#Gera randomicamente cada atributo
 forca = gerar_atributo()
 destreza = gerar_atributo()
 constituicao = gerar_atributo()
@@ -88,13 +143,36 @@ inteligencia = gerar_atributo()
 sabedoria = gerar_atributo()
 carisma = gerar_atributo()
 
+#Cria modificadores dos atributos
+#Vou fazer depois
+
+
+
 print(
+    "📜 Basico do personagem \n"
+    f"Nome: {nome}  \n"
+    f"Raça: {raca_escolhida} \n"
+    f"Classe: {classe_escolhida} \n"
+    f"Alinhamento: {alinhamento_escolhido} \n"
+    f"Antecedente: {antecedente_escolhido} \n\n"
+    
+    "🛡️ Atributos \n"
     f"FOR ({forca}) \n"
     f"DES ({destreza}) \n"
     f"CON ({constituicao}) \n"
     f"INT ({inteligencia}) \n"
     f"SAB ({sabedoria}) \n"
     f"CAR ({carisma}) \n"
+    
+    "Bônus de Proficiência: +2 \n\n"
+    
+    "💥 Perícias (proficiente em:) \n"
+    f"Escolhidas: {periciasEscolhidas} \n\n"
+    
+    "🗡️ Equipamento Inicial \n"
+    f"{equipamentoEscolhido}"
+    
+    f"❤️ Pontos de Vida: {10 + constituicao}"
 )
 
 #Combate 
